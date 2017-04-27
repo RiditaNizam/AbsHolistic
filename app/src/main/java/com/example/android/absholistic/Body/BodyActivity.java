d9c99175a58662c77ccda45f2869b7551a47d7d1
RiditaNizam <riditan@gmail.com>
2017-04-27 00:06:44 -0400

    Added buttons

--- app/src/main/java/com/example/android/absholistic/Body/BodyActivity.java
+++ app/src/main/java/com/example/android/absholistic/Body/BodyActivity.java
@@ -1,7 +1,11 @@
 package com.example.android.absholistic.Body;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
 import com.example.android.absholistic.R;
 
 public class BodyActivity extends AppCompatActivity {
@@ -11,4 +15,23 @@ public class BodyActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_body);
     }
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImageView.setImageBitmap(imageBitmap);
+        }
+    }
+
 }

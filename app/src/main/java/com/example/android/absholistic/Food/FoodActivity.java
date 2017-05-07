e642a58888d444000eb05b4b763d207f1c532c60
RiditaNizam <riditan@gmail.com>
2017-05-06 21:14:47 -0400

    Added buttons for meal ideas

--- app/src/main/java/com/example/android/absholistic/Food/FoodActivity.java
+++ app/src/main/java/com/example/android/absholistic/Food/FoodActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.example.android.absholistic.R;
 
@@ -13,13 +14,19 @@ public class FoodActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food);
+
+        Button mealideas = (Button) findViewById(R.id.mealideas);
+        mealideas.setOnClickListener(FoodActivity.this);
+
     }
 
-    public void sendMessage(View view) {
-        Intent intent = new Intent(FoodActivity.this, ToActivity.class);
-        startActivity(intent);
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.mealideas:
+                startActivity(new Intent(FoodActivity.this, MealIdeas.class));
+                break;
+        }
     }
-
-
 
 }
--- app/src/main/java/com/example/android/absholistic/Food/ToActivity.java
+++
@@ -1,8 +0,0 @@
-package com.example.android.absholistic.Food;
-
-/**
- * Created by ridita on 4/23/17.
- */
-
-class ToActivity {
-}

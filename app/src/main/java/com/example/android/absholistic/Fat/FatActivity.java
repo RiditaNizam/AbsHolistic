609ec86be7947dc1863575d34d20fa67173962cb
RiditaNizam <riditan@gmail.com>
2017-05-08 21:01:45 -0400

    Added activities to fat package

--- app/src/main/java/com/example/android/absholistic/Fat/FatActivity.java
+++ app/src/main/java/com/example/android/absholistic/Fat/FatActivity.java
@@ -1,8 +1,13 @@
 package com.example.android.absholistic.Fat;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.example.android.absholistic.Food.FoodActivity;
+import com.example.android.absholistic.MainActivity;
 import com.example.android.absholistic.R;
 
 public class FatActivity extends AppCompatActivity {
@@ -11,5 +16,17 @@ public class FatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fat);
+
+        Button anatomyandphysiologybutton = (Button) findViewById(R.id.anatomyandphysiology);
+        anatomyandphysiologybutton.setOnClickListener(FatActivity.this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.anatomyandphysiology:
+                startActivity(new Intent(FatActivity.this, AnatomyandPhysiology.class));
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/example/android/absholistic/Fat/FatCalipers.java
@@ -0,0 +1,15 @@
+package com.example.android.absholistic.Fat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.android.absholistic.R;
+
+public class FatCalipers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fat_calipers);
+    }
+}
